3fe120ff2a9d001c591fbd588d4060e855f212bf
James Scott <JamesScott96@live.co.uk>
2020-06-24 17:49:41 +0100

    initial commit

---
+++ src/main/java/com/springproject/scottishlocationapi/ScottishLocationApiApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.scottishlocationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScottishLocationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScottishLocationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springproject/scottishlocationapi/ScottishLocationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.scottishlocationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScottishLocationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
